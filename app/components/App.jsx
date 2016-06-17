69e7214492d5c906a7772b0aaba494ce847c097c
joshianuj <joshi.anujo7@gmail.com>
2016-06-17 15:13:25 +0545

    Hello world:

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-
-//components
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
-
 export default class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Header/>
-        <div>Content</div>
-        <div>Footer</div>
-      </div>
+     <div>'hello world'</div>
     )
   }
 }
